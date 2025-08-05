605b4cce12bb14cce2611756843635620945b793
Akshat0811 <singhakshat875@gmail.com>
2025-08-06 00:25:23 +0530

    turning ideas part in process

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,34 @@
-import "../css/Home.css";
+<div className="sub-heading">
+  <svg width="400" height="250" className="circular-text">
+    <defs>
+      <path id="curveTop" d="M 50,130 A 150,100 0 0 1 350,130" fill="none" />
+      <path id="curveBottom" d="M 50,230 A 150,100 0 0 0 350,230" fill="none" />
+    </defs>
 
-const Home = () => {
-  return (
-    <div className="hero-section">
-      <div className="main-heading">
-        <h1>Creative Animal</h1>
-      </div>
-      <div className="sub-heading">
-        <svg width="400" height="250" className="circular-text">
-          <defs>
-            <path
-              id="curveTop"
-              d="M 50,180 A 150,100 0 0 1 350,180"
-              fill="none"
-            />
-            <path
-              id="curveBottom"
-              d="M 50,180 A 100,100 0 0 0 350,180"
-              fill="none"
-            />
-          </defs>
+    <text fill="white" fontSize="70" fontWeight="bold">
+      <textPath href="#curveTop" startOffset="50%" textAnchor="middle">
+        TURNING
+      </textPath>
+    </text>
 
-          <text fill="white" fontSize="70" fontWeight="bold">
-            <textPath href="#curveTop" startOffset="50%" textAnchor="middle">
-              TURNING
-            </textPath>
-          </text>
+    <text
+      fill="white"
+      fontSize="70"
+      fontWeight="bold"
+      letterSpacing="8"
+    >
+      <textPath href="#curveBottom" startOffset="50%" textAnchor="middle">
+        IDEAS
+      </textPath>
+    </text>
+  </svg>
 
-          <text fill="white" fontSize="70" fontWeight="bold">
-            <textPath href="#curveBottom" startOffset="50%" textAnchor="middle">
-              IDEAS
-            </textPath>
-          </text>
-        </svg>
-      </div>
-    </div>
-  );
-};
-
-export default Home;
+  <div className="brand-info">
+    <h2>INTO ICONIC BRANDS</h2>
+    <p>
+      At Creative Animals, we believe that creativity is the key to building unforgettable brands.
+      As a full-service branding and advertising agency, we combine innovative ideas with strategic
+      insights to craft powerful, compelling narratives that resonate with your audience.
+    </p>
+  </div>
+</div>
